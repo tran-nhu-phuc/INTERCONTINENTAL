436037eaea5300433c2cae052f6ce69ed7cb5ced
trannhutai <trannhutai02112004>
2024-03-07 14:58:47 +0700

    update entity

--- servers/src/entities/index.ts
+++ servers/src/entities/index.ts
@@ -12,41 +12,41 @@ import User from "./user.table";
 import Voucher from "./voucher.table";
 
 const createTable = () => {
-  // User.sync().then(() => {
-  //   console.log("User create ok");
-  // });
-  // Booking.sync().then(() => {
-  //   console.log("Booking create ok");
-  // });
-  // CustomerInFo.sync().then(() => {
-  //   console.log("CustomerInFo create ok");
-  // });
-  // Room.sync().then(() => {
-  //   console.log("Room create ok");
-  // });
-  // Point.sync().then(() => {
-  //   console.log("Point create ok");
-  // });
-  // Category.sync().then(() => {
-  //   console.log("Category create ok");
-  // });
-  // ImageRoom.sync().then(() => {
-  //   console.log("ImageRoom create ok");
-  // });
-  // Comment.sync().then(() => {
-  //   console.log("Comment create ok");
-  // });
-  // Ratting.sync().then(() => {
-  //   console.log("Ratting create ok");
-  // });
-  // Voucher.sync().then(() => {
-  //   console.log("Voucher create ok");
-  // });
-  // Admin.sync().then(() => {
-  //   console.log("Admin create ok");
-  // });
-  // Like.sync().then(() => {
-  //   console.log("Like create ok");
-  // });
+  User.sync().then(() => {
+    console.log("User create ok");
+  });
+  Booking.sync().then(() => {
+    console.log("Booking create ok");
+  });
+  CustomerInFo.sync().then(() => {
+    console.log("CustomerInFo create ok");
+  });
+  Room.sync().then(() => {
+    console.log("Room create ok");
+  });
+  Point.sync().then(() => {
+    console.log("Point create ok");
+  });
+  Category.sync().then(() => {
+    console.log("Category create ok");
+  });
+  ImageRoom.sync().then(() => {
+    console.log("ImageRoom create ok");
+  });
+  Comment.sync().then(() => {
+    console.log("Comment create ok");
+  });
+  Ratting.sync().then(() => {
+    console.log("Ratting create ok");
+  });
+  Voucher.sync().then(() => {
+    console.log("Voucher create ok");
+  });
+  Admin.sync().then(() => {
+    console.log("Admin create ok");
+  });
+  Like.sync().then(() => {
+    console.log("Like create ok");
+  });
 };
 export default createTable;
